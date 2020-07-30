547d4053a3e7e5049d1932ef5cd6862da4037bbb
namchokGithub <pech4751@gmail.com>
2020-07-30 09:11:58 +0700

    Add template login and register

---
+++ AdminLTE-with-ASP.NET/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AdminLTE_with_ASP.NET.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AdminLTE_with_ASP.NET.Areas.Identity.IdentityHostingStartup))]
+namespace AdminLTE_with_ASP.NET.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
